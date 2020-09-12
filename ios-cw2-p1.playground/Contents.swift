87383c2a6acdfc7f37a809aed5a25fb2072f7a43
JulyLife-C60 <ghanima.sabti@gmail.com>
2020-09-12 20:35:43 +0300

    Ios-cw2

---
+++ ios-cw2-p1.playground/Contents.swift
@@ -0,0 +1,9 @@
+let family = ["Shaikha" , "Faisal" , "Ghanima" , "Dhari"]
+var evens = [2,4,6,8,10,12]
+var grades = [81.2 , 83.3 , 80.4]
+var  trueFalse = [false , false , false , true]
+print("Family:", family[0] , family[3] )
+print("evens:", evens[0] , evens[5] )
+print("grades:", grades[0] , grades[2] )
+print("trueFalse:", trueFalse[0] , trueFalse[3] )
+
---
+++ ios-cw2-p2.playground/Contents.swift
@@ -0,0 +1,9 @@
+var fruits = ["mango" , "orange" , "potato" , "peach"]
+print(fruits[0] , fruits[3])
+fruits.append("Strawberry")
+print(fruits)
+fruits.remove(at: 2)
+print(fruits)
+fruits += ["Banana" , "watermelon" , "pineapple" , "kiwi"]
+print(fruits)
+fruits.removeAll()
